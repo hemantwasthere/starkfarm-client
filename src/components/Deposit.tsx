c6401157b04663e6574f14c60df8ff3e291adc07
akiraonstarknet <zkdice@proton.me>
2025-08-29 14:15:32 +0530

    build fix

--- src/components/Deposit.tsx
+++ src/components/Deposit.tsx
@@ -275,6 +275,10 @@ function InternalDeposit(props: DepositProps) {
         ? convertToMyNumber(investedSummary)
         : MyNumber.fromZero(),
       tokenAddr: props.strategy.settings.quoteToken.address.address,
+      request_id: `req_${new Date().getTime()}`,
+      block_number: 0,
+      txIndex: 0,
+      eventIndex: 0,
     };
   }, [props, investedSummary, isDeposit]);
 
