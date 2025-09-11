94a0e63d51deb1e226127e6573bac2eff91183d0
Oche <ejembioche50@gmail.com>
2025-09-11 11:27:59 +0100

    chore: toast ui improvements

--- src/app/strategy/[strategyId]/_components/Strategy.tsx
+++ src/app/strategy/[strategyId]/_components/Strategy.tsx
@@ -128,7 +128,7 @@ function HoldingsAndEarnings({
       width={'100%'}
       justifyContent={'space-between'}
       gap={2}
-      minWidth={'400px'}
+      minWidth={'300px'}
     >
       <Box padding={'16px'} bg="mycard" width={'100%'} borderRadius={'lg'}>
         <Text color={'text_secondary'}>
