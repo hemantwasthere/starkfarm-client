e2495f41911a210e4897ee79bdaeef98f64cf64a
akiraonstarknet <zkdice@proton.me>
2025-09-24 23:02:24 +0530

    activate all strats

--- src/store/strategies.atoms.tsx
+++ src/store/strategies.atoms.tsx
@@ -368,16 +368,16 @@ export function getStrategies() {
 
   // undo
   const strategies: IStrategy<any>[] = [
-    // autoStrkStrategy,
-    // autoUSDCStrategy,
-    // deltaNeutralMMUSDCETH,
-    // deltaNeutralMMETHUSDC,
-    // deltaNeutralMMSTRKETH,
-    // deltaNeutralMMETHUSDCReverse,
-    // deltaNeutralxSTRKSTRK,
-    // ...vesuRebalanceStrats,
+    autoStrkStrategy,
+    autoUSDCStrategy,
+    deltaNeutralMMUSDCETH,
+    deltaNeutralMMETHUSDC,
+    deltaNeutralMMSTRKETH,
+    deltaNeutralMMETHUSDCReverse,
+    deltaNeutralxSTRKSTRK,
+    ...vesuRebalanceStrats,
     ...ekuboCLStrats,
-    // ...evergreenStrategies,
+    ...evergreenStrategies,
     ...hyperLSTStrategies,
     // xSTRKStrategy,
   ];
